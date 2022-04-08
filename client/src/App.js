ef8920d32c104302c3fd8b4aca292fcab6022dac
jedwed <83894732+jedwed@users.noreply.github.com>
2022-04-09 00:02:00 +1000

    Sign up now stores emails and authHashes into sqlite3 database

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,49 @@
 import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import PBKDF2 from "crypto-js/pbkdf2";
+import AES from "crypto-js/aes";
+import Hex from "crypto-js/enc-hex";
 
 function App() {
-  const signupUser = async (email, password, confirmPassword) => {
-    const newUser = { email, password };
+  const [activeMasterKey, setActiveMasterKey] = useState("");
+  const [activeAuthHash, setActiveAuthHash] = useState("");
+
+  const deriveMasterKey = (email, masterPassword) => {
+    return PBKDF2(masterPassword, email, {
+      iterations: 100000,
+      keySize: 256 / 32,
+    });
+  };
+
+  const deriveAuthHash = (masterKey, masterPassword) => {
+    return PBKDF2(masterKey, masterPassword, {
+      iterations: 10000,
+      keySize: 256 / 32,
+    }).toString(Hex);
+  };
+
+  const signupUser = async (email, password) => {
+    console.log(email, password);
+    const newMasterKey = deriveMasterKey(email, password);
+    setActiveMasterKey(newMasterKey);
+    const newAuthHash = deriveAuthHash(newMasterKey, password);
+    setActiveAuthHash(newAuthHash);
+
+    const requestBody = {
+      email,
+      authHash: newAuthHash,
+    };
     const response = await fetch("http://127.0.0.1:5000/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       mode: "cors",
-      body: JSON.stringify(newUser),
+      body: JSON.stringify(requestBody),
     });
     return true;
   };
--- server/src/server.py
+++ server/src/server.py
@@ -1,12 +1,31 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import sqlite3
+from json import dumps
 
 app = Flask(__name__)
 CORS(app)
 
+DATABASE = 'data.db'
+
+def query_db(query, args=()):
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    res = cur.execute(query, args).fetchall()
+    con.commit()
+    con.close()
+    return res
+
+def init_db():
+    query_db("CREATE TABLE IF NOT EXISTS users (email text, auth_hash text)")
+
 @app.route("/register", methods=['POST'])
 def user_register():
-    print(request.get_json())
+    email = request.get_json()['email']
+    auth_hash = request.get_json()['authHash'] 
+    query_db("INSERT INTO users VALUES (?, ?)", (email, auth_hash))
+    a = query_db("SELECT * FROM users")
+    print(dumps(a))
     return "Hello COMP6841! :)"
 
 @app.route("/login", methods=['POST'])
@@ -18,4 +37,5 @@ def user_login():
 # @app.route("/")
 
 if __name__ == "__main__":
-    app.run()
+    init_db()
+    app.run(debug=True)
